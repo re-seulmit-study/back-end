627ed7495d2d66dbc5dc0ef57f16e5cfbfe3f72a
muchluvu <rorrxr@naver.com>
2024-12-03 21:29:53 +0900

    chore : 프로젝트 생성

---
+++ src/main/java/com/minjyeon/sulmit/SulmitpjApplication.java
@@ -0,0 +1,13 @@
+package com.minjyeon.sulmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulmitpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SulmitpjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minjyeon/sulmit/SulmitpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjyeon.sulmit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulmitpjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
